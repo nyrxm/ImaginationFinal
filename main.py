4a273fd61d607ca03172dbc302b4bd41ae07285f
nyrxm <123412270+nyrxm@users.noreply.github.com>
2023-04-04 07:17:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ Label_is_read = False
 Paper_is_checked = False
 Realise_No_Exam = False
 
-NPC = 'DAD'
+NPC = 'DAD:'
 
 program_state = 'START'
 
 p5.imageMode(p5.CENTER)
 p5.textAlign(p5.CENTER)
+font = p5.loadFont('Terminal.ttf')
+font2 = p5.loadFont('helvetica_medium.ttf')
 
 AnomolyImg = p5.loadImage('Anomoly.png');
-BadendImg = p5.loadImage('Badend.png');
+BadendImg = p5.loadImage('BadendV2.png');
 CowImg = p5.loadImage('Cow.png');
 DadImg = p5.loadImage('Dad.png');
 DrivingImg = p5.loadImage('Driving.png');
@@ -64,11 +66,15 @@ def draw():
     p5.background(255)
 
     p5.fill(255)
+    p5.textFont(font2)
+    p5.textSize(12)
     
     if(program_state == 'START'):
         p5.image(StartImg, 150, 150)
-        p5.text('CLICK TO START', 150, 280)
-        p5.text('Hmm...I love boba tea...', 150, 20)
+        p5.textSize(18)
+        p5.text('CLICK TO START', 150, 45)
+        p5.textSize(12)
+        p5.text('Hmm...I love boba tea...', 150, 280)
     elif(program_state == 'S01'):
         p5.image(StartImg, 150, 150)
         p5.text('VOICE: How’s your drink?', 150, 280)
@@ -86,7 +92,7 @@ def draw():
         p5.text('[Can’t See Clearly]', 150, 280)
     elif(program_state == 'S04'):
         p5.image(DadImg, 150, 150)
-        p5.text('DAD:They didnt ask me?', 150, 280)
+        p5.text('DAD: They didnt ask me?', 150, 280)
     elif(program_state == 'S05'):
         p5.image(DadImg, 150, 150)
         p5.text('DAD: Lets go pick up your sister.', 150, 280)
@@ -98,10 +104,10 @@ def draw():
         p5.text('[Can’t See Clearly]', 150, 280)
     elif(program_state == 'S07'):
         p5.image(RoadImg, 150, 150)
-        p5.text('Drive slower...', 150, 280)
+        p5.text(str(NPC) + ' Drive slower...', 150, 280)
     elif(program_state == 'S08'):
         p5.image(DrivingImg, 150, 150)
-        p5.text('Whats that on the road?', 150, 280)
+        p5.text(str(NPC) + ' Whats that on the road?', 150, 280)
     elif(program_state == 'S08C'):
         p5.image(AnomolyImg, 150, 150)
         p5.image(S08C, 150, 150)
@@ -113,15 +119,16 @@ def draw():
         p5.text('We have to run!', 150, 280)
     elif(program_state == 'S11'):
         p5.image(WoodsImg, 150, 150)
-        p5.text('Into the woods...!', 150, 280)
+        p5.text(str(NPC) + ' Into the woods...!', 150, 280)
     elif(program_state == 'S12'):
         p5.image(Zombies2Img, 150, 150)
         p5.text('They are catching up!', 150, 280)
     elif(program_state == 'BE'):
         p5.image(BadendImg, 150, 150)
+        p5.text('NOOOooooooOOoo————!!', 150, 280)
     elif(program_state == 'S13'):
         p5.image(CowImg, 150, 150)
-        p5.text('Do you know why does a cow have four stomachs?', 150, 280)
+        p5.text(str(NPC) + ' Do you know a cow have four stomachs?', 150, 280)
     elif(program_state == 'S14'):
         p5.image(QuestionsImg, 150, 150)
         p5.text('I remember the teacher talking about this in lecture...', 150, 280)
@@ -155,7 +162,7 @@ def draw():
         p5.text('Can’t see clearly...', 150, 280)
     elif(program_state == 'S21'):
         p5.image(RoadImg, 150, 150)
-        p5.text('Are you ready for the exam?', 150, 260)
+        p5.text(str(NPC) + ' Are you ready for the exam?', 150, 260)
     elif(program_state == 'S21C'):
         p5.image(RoadImg, 150, 150)
         p5.image(S21C, 150, 150)
@@ -190,10 +197,17 @@ def draw():
         p5.text('What a dream...', 150, 260)
     elif(program_state == 'HE'):
         p5.image(HEImg, 150, 150)
+        p5.textSize(24)
+        p5.fill(214, 128, 40)
         p5.text('Congratulation!', 150, 150)
+        p5.fill(0)
+        p5.textSize(12)
         p5.text('You have successfully woken up', 150, 250)
         p5.text('from the endless nightmare.', 150, 270)
-        p5.text('Click to restart.', 150, 50)
+        p5.textSize(15)
+        p5.fill(100, 100, 100)
+        p5.text('Click to Restart', 150, 175)
+        p5.fill(255)
         
 
 def keyPressed(event):
@@ -222,8 +236,10 @@ def mouseReleased(event):
     elif(program_state == 'S01C'):
         if(p5.mouseX < 254) and (p5.mouseX > 45) and (p5.mouseY > 46) and (p5.mouseY < 105):
             program_state = 'S02'
+            NPC = 'DAD:'
         elif(p5.mouseX < 254) and (p5.mouseX > 45) and (p5.mouseY > 124) and (p5.mouseY < 184):
             program_state = 'S22'
+            NPC = 'LYNN:'
     elif(program_state == 'S02'):
         program_state = 'S02C'
     elif(program_state == 'S02C'):
